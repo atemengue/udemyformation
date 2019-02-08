341287eb76da47b3042aa5879f699132c20efa86
atemengue <moafembe@gmail.com>
2019-02-08 21:53:05 +0100

    using portals

--- src/streams/StreamDelete.js
+++ src/streams/StreamDelete.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react'
+import Modal from '../components/Modal';
+
+const actions = (
+  <>
+    <button className="ui button negative">Delete</button>
+    <button className="ui button">Cancel</button>
+  </>
+)
 
 class StreamDelete extends Component {
   render() {
     return (
       <div>
         StreamDelete
+        <Modal 
+          title="Delete Stream"
+          content = "Are you sure you want to delete this stream"
+          actions={actions}
+        />
       </div>
-    )
+    );
   }
 }
 
