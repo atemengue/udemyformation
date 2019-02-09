98f5a91b57ec02260be92dea07926965e6b2d180
atemengue <moafembe@gmail.com>
2019-02-09 12:52:51 +0100

    setup context system

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react'
 import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
+
 
 class Button extends Component {
   static contextType = LanguageContext;
+
+  renderSubmit(value) {
+    return value === 'english' ? 'Submit': 'Voorelggen';
+  }
 
   render() {
     console.log(this.context);
     const text = this.context === 'english' ? 'submit' : 'Voorelggen';
     return (
-      <button className="ui primary">{text}</button>
+      <ColorContext.Consumer>
+      {(color) => 
+        <button className={`ui button ${color}`}>
+          <LanguageContext.Consumer>
+            {(value) => this.renderSubmit(value)}
+          </LanguageContext.Consumer>
+        </button>
+      }
+      </ColorContext.Consumer>
     );
   }
 }
