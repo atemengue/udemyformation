fef58cf795e9bb83fabd525ed20f94bde9d10025
atemengue <moafembe@gmail.com>
2019-02-10 07:15:13 +0100

    useEffect refactoring

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -4,16 +4,28 @@ import axios from 'axios';
 const ResourceList =  ({ resource }) => {
   const [resources, setResources ] = useState([]);
 
-  const fetchResource =  async () => {
-    const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`)
+  // const fetchResource =  async () => {
+  //   const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`)
 
-   setResources(response.data)
-  }
+  //  setResources(response.data) 
+  // }
 
   useEffect(() => {
-    fetchResource(resource)
-  }, [])
+    (async () => {
+      const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`)
+  
+     setResources(response.data) 
+    })(resource);
+  },[resource])
+
+  // useEffect(() => {
+  //   fetchResource(resource)
+  // }, [resource])
+
     return (<div>{resources.length}</div>
     )
 }
+
 export default  ResourceList;
+
+//definition et invocation immediate des fonctions.
